db48a8d728e351bc14c30a4792b1d347e1a171d9
kayongest <kayonga70@outlook.com>
2025-08-29 00:16:48 +0200

    Linking urls to the correct path to index files

--- inventory_management_system/inventory/views.py
+++ inventory_management_system/inventory/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+# Basic homepage with - navbar, buttons for login and signup etc.
+
+class Index(TemplateView):
+    template_name = 'inventory/index.html'
--- inventory_management_system/inventory_management_system/settings.py
+++ inventory_management_system/inventory_management_system/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # add my inventory 
+    'inventory',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
