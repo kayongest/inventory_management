9a099a32facdf79d33b54dc2641070b01f6e2353
kayongest <kayonga70@outlook.com>
2025-08-31 19:46:17 +0200

    Linked all pages - Login, Sign up and Home

--- inventory_management_system/inventory/urls.py
+++ inventory_management_system/inventory/urls.py
@@ -1,13 +1,10 @@
-from django.contrib import admin
 from django.urls import path
-from .views import Index, SignUpView
+from .views import Index, SignUpView, LogoutView  
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', Index.as_view(), name='index'),
     path('signup/', SignUpView.as_view(), name='signup'),
-    path('login/', auth_views.LoginView.as_view(template_name='inventory/login.html', name='login')),
-    path('logout/', auth_views.LogoutView.as_view(template_name='inventory/logout.html', name='logout')),
-    
-
+    path('login/', auth_views.LoginView.as_view(template_name='inventory/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),  # Use your custom LogoutView
 ]
--- inventory_management_system/inventory/views.py
+++ inventory_management_system/inventory/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView, View
-from django.contrib.auth import authenticate, login
-from .forms import UserRegisterForm  
+from django.contrib.auth import authenticate, login, logout  # Add logout import
+from .forms import UserRegisterForm
 
 # Basic homepage with - navbar, buttons for login and signup etc.
-
 class Index(TemplateView):
     template_name = 'inventory/index.html'
 
@@ -14,18 +13,18 @@ class SignUpView(View):
         return render(request, 'inventory/signup.html', {'form': form})
     
     def post(self, request):
-        # This form contains all the data the user submitted
         form = UserRegisterForm(request.POST)
-
-        # Checking if the form is valid
         if form.is_valid():
             form.save()
-            user = authenticate(  
-                username = form.cleaned_data['username'],
-                password = form.cleaned_data['password1']
+            user = authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password1']
             )
-
             login(request, user)
             return redirect('index')
-        
         return render(request, 'inventory/signup.html', {'form': form})
+
+class LogoutView(View):
+    def post(self, request):  
+        logout(request)
+        return redirect('index')
