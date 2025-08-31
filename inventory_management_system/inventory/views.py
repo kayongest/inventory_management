71793e94d2cb60abe5b19c61347eda8b2171b1a4
kayongest <kayonga70@outlook.com>
2025-08-31 16:46:21 +0200

    Added a signup page

--- inventory_management_system/inventory/views.py
+++ inventory_management_system/inventory/views.py
@@ -1,7 +1,29 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView, View
+from djabngo.contrib.auth import authenticate, login
 
 # Basic homepage with - navbar, buttons for login and signup etc.
 
 class Index(TemplateView):
     template_name = 'inventory/index.html'
+
+class SignUpView(View):
+    def get(self, request):
+        form = UserRegisterForm()
+        return render(request, 'inventory/signup.html', {'form'. form})
+    def post(self, request):
+        # This form contains all the data the user submitted
+        form = UserRegisterForm(request.POST)
+
+        # Checking if the form is valid
+        if form.is_valid():
+            form.save()
+            User = authenticate(
+                username = form.cleaned_data['username'],
+                password = form.cleaned_data['password1']
+            )
+
+            login(request, user)
+            return redirect('index')
+        
+        return render(request, 'inventory/signup.html', {'form': form})
