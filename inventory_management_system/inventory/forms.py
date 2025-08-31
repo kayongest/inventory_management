18797590033ea713ecd488a205db3f546a90983d
kayongest <kayonga70@outlook.com>
2025-08-31 18:01:49 +0200

    Signup function working as well as redirection (user)

--- inventory_management_system/inventory/forms.py
+++ inventory_management_system/inventory/forms.py
@@ -2,10 +2,9 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
-class UserRegisterFrom(UserCreationForm):
-    # created an email filed coz it doesn't come by default
-    email = forms.EmailField()    
-
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
--- inventory_management_system/inventory/views.py
+++ inventory_management_system/inventory/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView, View
-from djabngo.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login
+from .forms import UserRegisterForm  
 
 # Basic homepage with - navbar, buttons for login and signup etc.
 
@@ -10,7 +11,8 @@ class Index(TemplateView):
 class SignUpView(View):
     def get(self, request):
         form = UserRegisterForm()
-        return render(request, 'inventory/signup.html', {'form'. form})
+        return render(request, 'inventory/signup.html', {'form': form})
+    
     def post(self, request):
         # This form contains all the data the user submitted
         form = UserRegisterForm(request.POST)
@@ -18,7 +20,7 @@ class SignUpView(View):
         # Checking if the form is valid
         if form.is_valid():
             form.save()
-            User = authenticate(
+            user = authenticate(  
                 username = form.cleaned_data['username'],
                 password = form.cleaned_data['password1']
             )
